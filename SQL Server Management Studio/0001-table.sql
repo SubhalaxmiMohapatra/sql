392443efbcfd6dd79df835f7a816931c22096904
Subhalaxmi Mohapatra <81293609+SubhalaxmiMohapatra@users.noreply.github.com>
2022-06-17 18:20:14 +0530

    Add files via upload

---
+++ SQL Server Management Studio/0001-table.sql
@@ -0,0 +1,14 @@
+create database employee
+
+
+use employee 
+create table emp_details
+(
+empid int primary key,emp_name varchar(50),emp_salary varchar(50)
+)
+
+select * from emp_details
+
+insert into emp_details values(1,'subhalaxmi mohapatra','15000')
+
+select * from emp_details
